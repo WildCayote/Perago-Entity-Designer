ca74de171a20419481048db97382f027d2599947
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-04-29 02:59:44 -0700

    Added import Objet class and generateImports method to controllerGen service

--- src/code-gen/services/controller_gen.service.ts
+++ src/code-gen/services/controller_gen.service.ts
@@ -2,8 +2,81 @@ import { Injectable } from '@nestjs/common';
 import { Columns } from 'src/entities/column.entity';
 import { Model } from 'src/entities/model.entity';
 
+import * as handleBars from 'handlebars';
+
+class ImportObject {
+  constructor(
+    public module: string,
+    public dependency: string,
+  ) {}
+
+  equals(otherInstance: ImportObject) {
+    return (
+      this.module == otherInstance.module &&
+      this.dependency == otherInstance.dependency
+    );
+  }
+}
+
 @Injectable()
 export class ControllerGenService {
+  private imports = new Map<string, Set<ImportObject>>();
+
+  private async generateImports(
+    entities: Model[],
+    imports: Map<string, Set<ImportObject>>,
+  ) {
+    const dependencyModuleMap = new Map<
+      string,
+      Map<string, Map<string, ImportObject>>
+    >([]);
+    const importStatements = handleBars.compile(
+      `import { {{#each modules}}{{this}}{{#unless @last}}, {{/unless}}{{/each}} } from {{{dependency}}};\n
+\n`,
+    );
+    for (const entity of entities) {
+      const entityName = entity.name;
+      const entityImports = imports.get(entityName);
+      if (entityImports) {
+        for (const entityImport of entityImports) {
+          // check if the entity is being tracked , if not track it
+          if (!dependencyModuleMap[entityName])
+            dependencyModuleMap[entityName] = new Map<string, any>();
+
+          // check if the dependecy is being tracked , if not track it
+          if (!dependencyModuleMap[entityName][entityImport.dependency])
+            dependencyModuleMap[entityName][entityImport.dependency] =
+              new Set<string>([entityImport.module]);
+          else
+            dependencyModuleMap[entityName][entityImport.dependency].add(
+              entityImport.module,
+            );
+        }
+      }
+    }
+
+    let result = new Map<string, string>();
+
+    for (let entity of entities) {
+      const entityName = entity.name;
+      const dependencyMap = dependencyModuleMap[entityName];
+
+      if (dependencyMap) {
+        for (const dependency of Object.keys(dependencyMap)) {
+          const modules = dependencyMap[dependency];
+
+          if (!result[entityName]) result[entityName] = '';
+
+          result[entityName] += importStatements({
+            modules: modules,
+            dependency: dependency,
+          });
+        }
+      }
+    }
+    return result;
+  }
+
   async generateOutPut(entities: Model[], columns: Columns[]) {
     return 'Test controller output';
   }
