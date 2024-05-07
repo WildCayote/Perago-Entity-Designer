48345ae9a8c1d44b40a0207b97989ef99b96d786
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-05-06 22:55:32 -0700

    Removed project extraction handlers and controllers from the model module

--- src/model/model.controller.ts
+++ src/model/model.controller.ts
@@ -6,32 +6,13 @@ import {
   Param,
   Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 import { ModelService } from './model.service';
 import { CreateModelDto, UpdateModelDto } from './dto';
-import { Response } from 'express';
 
 @Controller({ path: 'projects', version: '1' })
 export class ModelController {
   constructor(private modelService: ModelService) {}
-
-  @Get(':projectId/extract')
-  async extractCode(@Param('projectId') projectId) {
-    const codeResponse = await this.modelService.extractModel(projectId);
-
-    return codeResponse;
-  }
-
-  @Get(':jobId/obtain')
-  async getResult(@Param('jobId') jobId, @Res() res: Response) {
-    const buffer = await this.modelService.obtainResult(jobId);
-
-    res.setHeader('Content-Disposition', 'attachment; filename="src.zip"');
-    res.setHeader('Content-Type', 'application/zip');
-
-    res.send(buffer);
-  }
 
   @Get(':id/models')
   getModels(@Param('id') projectId: string) {
--- src/model/model.service.ts
+++ src/model/model.service.ts
@@ -1,34 +1,17 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { Columns } from 'src/entities/column.entity';
 import { Model } from 'src/entities/model.entity';
 
 import { CreateModelDto, UpdateModelDto } from './dto';
 import { isInstance } from 'class-validator';
 
-import { Project } from 'src/entities/project.entity';
-
-import { PgBossService } from 'src/pg-boss/pg-boss.service';
-
 @Injectable()
 export class ModelService {
   constructor(
-    private pgBossService: PgBossService,
-
-    @InjectRepository(Project)
-    private projectRepository: Repository<Project>,
-
     @InjectRepository(Model)
     private modelRepositroy: Repository<Model>,
-
-    @InjectRepository(Columns)
-    private columnRepository: Repository<Columns>,
   ) {}
 
   async getModels(projectId: string) {
@@ -52,47 +35,6 @@ export class ModelService {
           "The model you were looking for doesn't exist!",
         );
       else if (isInstance(error, NotFoundException)) throw error;
-    }
-  }
-
-  async extractModel(projectId: string) {
-    try {
-      const project = await this.projectRepository.findOne({
-        where: { id: projectId },
-      });
-
-      // get all columns
-      const allColumns = await this.columnRepository.find();
-
-      // get all entities of the project
-      const entities = await this.modelRepositroy.find({
-        where: { projectId: projectId },
-      });
-
-      const entityIds = entities.map((entity) => entity.id);
-
-      // filter columns that belong to the proper entities, i.e entities that belong to the project
-      const columns = allColumns.filter((column) =>
-        entityIds.includes(column.modelId),
-      );
-
-      //testing pg-boss
-      const pgResponse = await this.pgBossService.addJob(entities, columns);
-
-      console.log(`Job Id recieved : ${pgResponse}`);
-
-      return { jobId: pgResponse };
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async obtainResult(jobId: string) {
-    try {
-      const result = await this.pgBossService.obtainResult(jobId);
-      return result;
-    } catch (error) {
-      throw new InternalServerErrorException(error);
     }
   }
 
